8ca293cddc17d47ca37dfcdefe81d14219ebb2cc
Baguimon <timothe.winkler@gmail.com>
2023-12-19 10:17:39 +0100

    wip - adding content fetcher

---
+++ src/app/content-fetcher/page.jsx
@@ -0,0 +1,22 @@
+// "use client"
+
+// import { useEffect } from "react";
+
+// const ContentFetcher = () => {
+//     const URL = "https://foaas.dev"
+
+//     useEffect(assync () => {
+//         const response = await fetch('${URL}/operations')
+//             .then((res) => res.json())
+//             .then((res) => console.log(data))
+//             .catch((err) => console.log(err));
+//     })
+
+//     return (
+//         <>
+//             <p>Some Data</p>
+//         </>
+//     );
+// };
+
+// export default ContentFetcher
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,12 +1,21 @@
+"use client"
+
+import { useAtom } from "jotai";
 import AddArticle from "@/components/articles/AddArticle";
+import { userAtom } from "@/globalState";
+import InsertName from "@/components/InsertName";
 
 const Dashboard = () => {
+    const [ user ] = useAtom(userAtom);
+
     return(
         <div>
             <h1>Dashboard</h1>
+            <p>Votre activité, {user.name}</p>
             <AddArticle />
+            <InsertName />
         </div>
-    )
-}
+    );
+};
 
 export default Dashboard;
